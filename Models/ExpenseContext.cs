e7bcedbc72230c3be27757dac05ba92ca5804477
Bogdan-cmd <bogdan.d.istrate@gmail.com>
2021-11-20 18:39:33 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using IstrateBogdan_Laborator5.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IstrateBogdan_Laborator5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
